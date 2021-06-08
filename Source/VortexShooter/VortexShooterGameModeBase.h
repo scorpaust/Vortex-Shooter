da1d254dd61de05339d841546c4b8dab359979d1
Dinis Costa <scorpaust@hotmail.com>
2021-06-08 23:48:59 +0100

    Git ignore file

---
+++ Source/VortexShooter/VortexShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VortexShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VORTEXSHOOTER_API AVortexShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
