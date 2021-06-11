3477986c55cb50040c4264d227ef6c256a32336e
Dinis Costa <scorpaust@hotmail.com>
2021-06-12 00:56:51 +0100

    Impact particles.

--- Source/VortexShooter/ShooterCharacter.h
+++ Source/VortexShooter/ShooterCharacter.h
@@ -73,6 +73,14 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
 	class UParticleSystem* MuzzleFlash;
 
+	/** Montage for firing the weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
+	class UAnimMontage* HipFireMontage;
+
+	/** Particles spawned upon bullet impact */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
+	UParticleSystem* ImpactParticles;
+
 public:
 
 	/** Returns CameraBoom subobject */
